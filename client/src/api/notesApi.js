8224d3d84f111e7a1d15fe561befbf908ad0de6e
Lingxiao Han <128201167+Lingxiao-Han@users.noreply.github.com>
2025-02-20 17:48:17 -0500

    Merge main update (#13)
    * updated development port
    
    * updated development port
    
    * updated backend server
    
    * updated url
    
    * Add CD pipeline for OpenShift deployment
    
    * Fix build context and switch to Deployment for rollout

--- client/src/api/notesApi.js
+++ client/src/api/notesApi.js
@@ -1,9 +1,8 @@
 // src/api/notesApi.js
 import axios from 'axios';
 
-
+//const API_BASE = "http://notes-backend-hanlingxiao2005-dev.apps.rm3.7wse.p1.openshiftapps.com/notes";
 const API_BASE = import.meta.env.VITE_API_URL
-
 
 export const getNotes = () => axios.get(API_BASE);
 export const getNoteById = (id) => axios.get(`${API_BASE}/${id}`);
