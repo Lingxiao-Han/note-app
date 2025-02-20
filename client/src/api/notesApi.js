fac07e640f63b0f08dae39d64a239a1dcb9a5180
Lingxiao Han <128201167+Lingxiao-Han@users.noreply.github.com>
2025-02-20 16:15:06 -0500

    updated development port (#8)

--- client/src/api/notesApi.js
+++ client/src/api/notesApi.js
@@ -1,7 +1,8 @@
 // src/api/notesApi.js
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_URL;
+//const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = "http://notes-backend:8000/notes";
 
 export const getNotes = () => axios.get(API_BASE);
 export const getNoteById = (id) => axios.get(`${API_BASE}/${id}`);
